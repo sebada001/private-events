fe93ce380dfbb8b949bfc79e70e46924c4526a88
Sebastian Schumacher <sebastianschumacher10@gmail.com>
2023-01-10 19:44:59 -0400

    relations in models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :events_created, foreign_key: :creator_id, class_name: "Event"
   has_many :event_attendances, foreign_key: :attendant_id
-  has_many :events_attended, through: :event_attendances, source: :attended_id
+  has_many :events_attended, through: :event_attendances, source: :attended
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
